758b6e6eb2ff321ff0815ee132ed1f5fa910fb95
Han LetsGo <han.lets.go@gmail.com>
2019-06-02 19:22:28 +0800

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: '挺韓 選總統 ｜ 後援網站',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

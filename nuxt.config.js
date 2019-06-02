0fe0aea84502b6579c5ff10323a9f45e02207f05
Han LetsGo <han.lets.go@gmail.com>
2019-06-02 16:25:54 +0800

    verify google search-console

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      {
+        name: 'google-site-verification',
+        content: 'TJKDeH5SB5WZD6Ispe3mTkWbxggXyMmx_YVnMjoE0xQ'
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
